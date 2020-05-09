b29a3eecd7fc0916287927ac17e04af913519085
IrenaKv <ik12664@my.bristol.ac.uk>
2020-05-09 14:03:26 +0200

    added Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/patientdatabase" component={PatientDatabase} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
